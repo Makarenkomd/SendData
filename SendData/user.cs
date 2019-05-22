0b8da202b7aced1515ef16838fb65c90a3de1fbd
diana <diatuaeva@gmail.com>
2019-05-22 10:54:28 +0300

    Создан класс user, для передачи данных в формы итогов

---
+++ SendData/user.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SendData
+{
+    class user
+    {
+        string name;
+        int score;
+        DateTime date;
+
+        public user(string nameUser, int scoreUser)
+        {
+            name = nameUser;
+            score = scoreUser;
+            date = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return name + "\t" + date + "\t" + score;
+        }
+
+    }
+}
